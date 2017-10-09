ac726118f34d860ea9b767e7d998cd5499b51ff8
James LeClair <james@pixelpro.co>
2017-10-08 20:15:38 -0400

    create tab section

--- interface/src/app.storyfile.js
+++ interface/src/app.storyfile.js
@@ -1,6 +1,15 @@
 import { storiesOf } from '@storybook/react'
+import { MemoryRouter } from 'react-router-dom'
 import Header from './Header'
 import React from 'react'
+import Tabs from './components/Tabs'
 
 storiesOf('Frame', module)
   .add('Header', () => <Header />)
+
+storiesOf('Tabs', module)
+  .add('presets', () =>
+    <MemoryRouter initialEntries={['/presets']}>
+      <Tabs />
+    </MemoryRouter>
+  )
---
+++ interface/src/components/Tabs.jsx
@@ -0,0 +1,37 @@
+import { colors, Flex } from '../styles'
+import { NavLink } from 'react-router-dom'
+import glamorous from 'glamorous'
+import React from 'react'
+
+const TabContainer = glamorous(Flex)({
+  height: 40,
+  padding: '0 20px',
+  borderBottom: `1px solid ${colors.borderColor}`,
+  ' a': {
+    fontSize: 12,
+    fontWeight: 500,
+    color: colors.text.greyMedium,
+    display: 'block',
+    lineHeight: '40px',
+    padding: '0 10px',
+    textDecoration: 'none',
+    marginRight: 15,
+    '&.active': {
+      color: colors.text.greyDark,
+      borderBottom: '2px solid #2A9EF5'
+    }
+  }
+})
+
+class Tabs extends React.Component {
+  render () {
+    return (
+      <TabContainer>
+        <NavLink to='/presets' activeClassName='active'>Preset Configurations</NavLink>
+        <NavLink to='/routes' activeClassName='active'>Routes</NavLink>
+      </TabContainer>
+    )
+  }
+}
+
+export default Tabs
