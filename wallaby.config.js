c9e5965d52c547923a5dbab4047ba4c67fb3e71a
James LeClair <james@pixelpro.co>
2018-03-29 12:14:20 -0400

    rebuild config loader with tests

--- wallaby.config.js
+++ wallaby.config.js
@@ -3,7 +3,8 @@ module.exports = function (wallaby) {
     files: [
       {pattern: 'interface/src/**/*.js'},
       {pattern: 'interface/src/**/*.spec.js', ignore: true},
-      {pattern: 'lib/*.js'}
+      {pattern: 'lib/**/*.js'},
+      {pattern: 'lib/**/*.spec.js', ignore: true}
     ],
 
     tests: [
@@ -19,7 +20,8 @@ module.exports = function (wallaby) {
     },
 
     compilers: {
-      'interface/src/**/*.js': wallaby.compilers.babel()
+      'interface/src/**/*.js': wallaby.compilers.babel(),
+      'lib/src/**/*.js': wallaby.compilers.babel()
     }
   }
 }
