9157242b2077f99cb563cb30c020fc7507321285
James LeClair <james@pixelpro.co>
2018-05-11 21:38:25 -0400

    added tests for presets

--- src/presets/index.js
+++ src/presets/index.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import Config from '../config/loader'
 import fs from 'fs-extra'
+import { ErrPresetNotFound } from '../errors'
 import Preset from './Preset'
 
 export class Presets {
@@ -21,38 +22,65 @@ export class Presets {
 
   _ensureFileExists (configFilePath) {
     if (!fs.existsSync(configFilePath)) {
-      // TODO throw error here
+      throw new ErrPresetNotFound()
     }
   }
 
-  _ensureFileIntegrity () {
-    // TODO figure out validations and perform
+  _buildFilePath (fileName, configDirectory = Config.configDirectory) {
+    return `${configDirectory}/presets/${fileName}.js`
   }
 
-  _buildFilePath (fileName) {
-    return `${Config.configDirectory}/presets/${fileName}.js`
-  }
-
-  _getPresetFileData ({ filePath, presets = require(path.resolve(filePath)) }) {
-    this._ensureFileIntegrity(presets)
+  _getPresetFileData ({ filePath, fileData = require(path.resolve(filePath)) }) {
+    const presets = []
+    fileData.forEach(({path, mode, ...methods}) => {
+      Object.keys(methods).forEach(method => {
+        presets.push(new Preset({ path, mode, method, data: methods[method] }))
+      })
+    })
     return presets
   }
 
   async loadPreset (presetName) {
-    let filePath = this._buildFilePath(presetName)
-    await this._ensureFileExists(filePath)
-
-    const presets = this._getPresetFileData({filePath})
-    presets.forEach(preset => {
-      let indexToOverwrite = this._presets.findIndex(row => row.path === preset.path && row.method === preset.method)
-      indexToOverwrite === -1
-        ? this._presets.push(new Preset(preset))
-        : this._presets[indexToOverwrite] = new Preset(preset)
-    })
+    const presets = await this.validateAndLoadPresetFile(presetName)
+    this.addPresetsToInternalCollection(presets)
   }
 
   async loadPresets (presetNames) {
+    const presetDataCollection = presetNames.map(async (name) => {
+      await this.validateAndLoadPresetFile(name)
+    })
 
+    const presets = this.combinePresets(presetDataCollection)
+    this.addPresetsToInternalCollection(presets)
+  }
+
+  addPresetsToInternalCollection (presets) {
+    presets.forEach(preset => {
+      let indexToOverwrite = this._presets.findIndex(row => row.path === preset.path && row.method === preset.method)
+      indexToOverwrite === -1 ? this._presets.push(preset) : this._presets[indexToOverwrite] = preset
+    })
+  }
+
+  async validateAndLoadPresetFile (presetName) {
+    let filePath = this._buildFilePath(presetName)
+    await this._ensureFileExists(filePath)
+    return this._getPresetFileData({filePath})
+  }
+
+  combinePresets (presetDataCollection) {
+    return presetDataCollection.reduce((acc, presetFile) => {
+      presetFile.forEach(preset => {
+        let existingIndex = acc.findIndex(row => row.path === preset.path && row.method === preset.method)
+        if (existingIndex > -1) {
+          acc[existingIndex].data = preset.mode === 'merge'
+            ? {...acc[existingIndex].data, ...preset.data}
+            : {...preset.data}
+        } else {
+          acc.push(preset)
+        }
+      })
+      return acc
+    }, [])
   }
 }
 
