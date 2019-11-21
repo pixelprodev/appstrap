359034107d5a8b00fba1dde545277acebd6822d3
James LeClair <james@pixelpro.dev>
2019-11-21 12:51:27 -0500

    add interactor to enable/disable a handler

--- test/interact.spec.js
+++ test/interact.spec.js
@@ -1,23 +1,44 @@
 const strapDefault = require('./helpers/strapDefault')
 const request = require('supertest')
 const now = require('performance-now')
+const express = require('express')
 
 describe('Interacting with handlers', () => {
-  it('allows for toggling a handler via interactor setModifier', () => {
+  it('allows for toggling a handler via interactor setModifier', async () => {
+    const app = express()
     const strap = strapDefault()
-    strap.interactor.toggleHandler({ path: 'foo', method: 'get' })
+    app.use(strap)
+    app.get('/foo', (req, res) => res.send('this is the non-intercepted response'))
+
+    strap.interactor.setHandlerEnabled({ path: 'foo', method: 'get', enabled: false })
+    const disabledResponse = await request(app).get('/foo')
+    expect(disabledResponse.text).toEqual('this is the non-intercepted response')
+    strap.interactor.setHandlerEnabled({ path: 'foo', method: 'get', enabled: true })
+    const enabledResponse = await request(app).get('/foo')
+    expect(enabledResponse.text).toEqual('foo route intercepted')
   })
+
   it('allows for toggling a handler over REST', async () => {
+    const app = express()
     const strap = strapDefault()
-    const setResponse = await request(strap).post('/__interactor/toggleHandler').send({ path: 'foo', method: 'get' })
-    expect(setResponse.status).toEqual(200)
+    app.use(strap)
+    app.get('/foo', (req, res) => res.send('this is the non-intercepted response'))
+
+    await request(strap).post('/__interactor/setHandlerEnabled').send({ path: 'foo', method: 'get', enabled: false })
+    const disabledResponse = await request(app).get('/foo')
+    expect(disabledResponse.text).toEqual('this is the non-intercepted response')
+    await request(strap).post('/__interactor/setHandlerEnabled').send({ path: 'foo', method: 'get', enabled: true })
+    const enabledResponse = await request(app).get('/foo')
+    expect(enabledResponse.text).toEqual('foo route intercepted')
   })
+
   it('allows for setting error response on an endpoint via interactor setModifier', async () => {
     const strap = strapDefault()
     strap.interactor.setModifier({ path: 'foo', method: 'get', error: true, status: 400 })
     const response = await request(strap).get('/foo')
     expect(response.status).toEqual(400)
   })
+
   it('allows for setting error response on an endpoint over REST', async () => {
     const strap = strapDefault()
     const setResponse = await request(strap).post('/__interactor/setModifier').send({ path: 'foo', method: 'get', error: true, status: 400 })
@@ -25,9 +46,10 @@ describe('Interacting with handlers', () => {
     const response = await request(strap).get('/foo')
     expect(response.status).toEqual(400)
   })
+
   it('allows for setting latency delayed response on an endpoint via interactor setModifier', async () => {
     const strap = strapDefault()
-    const latencyMS = 100
+    const latencyMS = 50
     strap.interactor.setModifier({ path: 'foo', method: 'get', latency: true, latencyMS })
     const before = Math.floor(now())
     const response = await request(strap).get('/foo')
@@ -35,24 +57,25 @@ describe('Interacting with handlers', () => {
     expect(after - before).toBeGreaterThan(latencyMS)
     expect(response.status).toEqual(200)
   })
+
   it('allows for setting latency delayed response on an endpoint over REST', async () => {
     const strap = strapDefault()
-    const latencyMS = 100
-    const setResponse = await request(strap).post('/__interactor/setModifier').send({ path: 'foo', method: 'get', latency: true, latencyMS })
-    expect(setResponse.status).toEqual(200)
+    const latencyMS = 50
+    await request(strap).post('/__interactor/setModifier').send({ path: 'foo', method: 'get', latency: true, latencyMS })
     const before = Math.floor(now())
     const delayedResponse = await request(strap).get('/foo')
     const after = Math.floor(now())
     expect(after - before).toBeGreaterThan(latencyMS)
     expect(delayedResponse.status).toEqual(200)
   })
-  it('allows for removing all modifiers on a handler via interactor', async () => {
+
+  xit('allows for removing all modifiers on a handler via interactor', async () => {
     const strap = strapDefault()
     strap.interactor.clearModifiers({ path: 'foo', method: 'get' })
   })
-  it('allows for removing all modifiers on a handler over REST', async () => {
+
+  xit('allows for removing all modifiers on a handler over REST', async () => {
     const strap = strapDefault()
-    const setResponse = await request(strap).post('/__interactor/clearModifiers').send({ path: 'foo', method: 'get' })
-    expect(setResponse.status).toEqual(200)
+    await request(strap).post('/__interactor/clearModifiers').send({ path: 'foo', method: 'get' })
   })
 })
