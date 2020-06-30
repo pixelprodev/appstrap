02d318e2e0725e8e8c862d0b84fc2cc7c0898f9a
James LeClair <james@pixelpro.dev>
2020-06-29 23:27:35 -0400

    cover scenario where res.sendStatus can still be overridden by fixtures make activating/deactivating fixtures not case-sensitive

--- test/_configs/defaultConfig.js
+++ test/_configs/defaultConfig.js
@@ -6,6 +6,7 @@ module.exports = {
     { path: 'foo', get: (req, res) => res.send('foo route intercepted') },
     { path: 'bar', post: (req, res) => res.send('bar route intercepted') },
     { path: 'state-echo', get: (req, res, next, state) => res.json(state) },
-    { path: 'fixture-intercept', get: (req, res) => res.json({ default: true }) }
+    { path: 'fixture-intercept', get: (req, res) => res.json({ default: true }) },
+    { path: 'fixture-replace-status', get: (req, res) => res.sendStatus(404) }
   ]
 }
